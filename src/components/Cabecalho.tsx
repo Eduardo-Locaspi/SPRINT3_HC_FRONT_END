ccecaec2f0e23c0887153588f9cedd2dd41e94bf
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-09-24 18:10:26 -0300

    (2) - Estrutura Básica - componentes
    O que foi feito? components basicos

---
+++ src/components/Cabecalho.tsx
@@ -0,0 +1,13 @@
+
+export default function Cabecalho() {
+  return (
+    <header className="flex flex-col items-center bg-white shadow-md">
+      <div className="flex items-center justify-center py-4">
+        <div className="w-16 h-16 rounded-full bg-gray-300 flex items-center justify-center text-gray-700 font-bold">
+          Logo
+        </div>
+      </div>
+
+    </header>
+  );
+}
---
+++ src/components/Navegacao.tsx
@@ -0,0 +1,16 @@
+
+
+export default function Navegacao(){
+
+
+    return ( //ideia -> centralizar items
+        <nav className="w-full">
+            <ul className="flex justify-center gap-10">
+                <li>Página Principal</li>
+                <li>Nossos Produtos</li>
+                <li>Sobre Nós</li>
+                
+            </ul>
+        </nav>
+    )
+}
