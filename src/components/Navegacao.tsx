63f82f0c913521bbc1692877c27d3387551b1d74
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-09-25 16:16:35 -0300

    (4) - v1 Navegação
    Oque foi feito? - BArra de Navegação básica

--- src/components/Navegacao.tsx
+++ src/components/Navegacao.tsx
@@ -4,12 +4,11 @@ export default function Navegacao(){
 
 
     return ( //ideia -> centralizar items
-        <nav className="w-full">
-            <ul className="flex justify-center gap-10">
-                <li>Página Principal</li>
-                <li>Nossos Produtos</li>
-                <li>Sobre Nós</li>
-                
+        <nav className="w-full bg-red-700 text-white">
+            <ul className="flex justify-center gap-20 font-medium py-3">
+                <li>HOME</li>
+                <li>SOLUÇÃO</li>
+                <li>SOBRE NÓS</li>
             </ul>
         </nav>
     )
