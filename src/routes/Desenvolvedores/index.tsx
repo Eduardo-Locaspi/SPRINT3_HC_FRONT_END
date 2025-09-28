642cdfde1d934760a932637fd0d9603391078919
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-09-28 18:25:20 -0300

    (9) v2 - Tela de Desenvolvedores
    O que foi feito? - Estruturação do layout da tela de desenvolvedores

--- src/routes/Desenvolvedores/index.tsx
+++ src/routes/Desenvolvedores/index.tsx
@@ -1,17 +1,74 @@
+import dudu from '../../assets/img/desenvolvedor_eduardo.jpg'
 
+export default function Integrantes() {
+  return (
+    <main className="py-6 px-12">
+      <h1 className="text-5xl text-center p-10 underline ">Desenvolvedores</h1>
 
-export default function Integrantes(){
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {/* Eduardo */}
+        <section className="bg-gray-100 text-gray-900 text-center rounded-md flex flex-col items-center border border-white">
+          <div className="p-4">
+            <h2 className="p-4 text-3xl">Eduardo Locaspi</h2>
+            <img src={dudu} alt="Foto do desenvolvedor Eduardo Locaspi" className="w-40 h-40 object-cover rounded-md"/>
 
-    return(
-        <main className="py-6 px-12">
-            <h1 className="text-4xl text-center p-10"> Desenvolvedores</h1>
+            <div className="p-5 flex flex-row justify-center gap-5">
 
-            <div className="grid grid-cols-3 gap-x-2">
-                <section className="bg-red-500 text-center rounded-md ">Eduardo Locaspi</section>
-                <section className="bg-red-400 text-center rounded-md">Natan Moraes</section>
-                <section className="bg-red-600 text-center rounded-md">Victor Lopes</section>
+              <a href="https://www.linkedin.com/in/eduardo-locaspi-14119934b/" target="_blank">
+                <img src="/img/linkedin.png" alt="LinkedIn" className="w-10" />
+              </a>
+              <a href="https://github.com/Eduardo-Locaspi" target="_blank">
+                <img src="/img/github.png" alt="GitHub" className="w-10" />
+              </a>
+
             </div>
-        </main>
-        
-    )
+          </div>
+        </section>
+
+        {/* Natan */}
+        <section className="bg-gray-100 text-gray-900 text-center rounded-md flex flex-col items-center border border-white">
+          <div className="p-4">
+            <h2 className="p-4 text-3xl">Natan Moraes</h2>
+            <img
+              src=""
+              alt="Foto do desenvolvedor Natan Moraes"
+              className="w-40 h-40 object-cover rounded-md"
+            />
+
+            <div className="p-5 flex flex-row justify-center gap-5">
+              <a
+                href="https://www.linkedin.com/in/natanfreitasdemoraes/"
+                target="_blank">
+                <img src="/img/linkedin.png" alt="LinkedIn" className="w-10" />
+              </a>
+              <a href="https://github.com/nfreitas2000" target="_blank">
+                <img src="/img/github.png" alt="GitHub" className="w-10" />
+              </a>
+            </div>
+          </div>
+        </section>
+
+        {/* Victor */}
+        <section className="bg-gray-100 text-gray-900 text-center rounded-md flex flex-col items-center">
+          <div className="p-4">
+            <h2 className="p-4 text-3xl">Victor Alves</h2>
+            <img
+              src=""
+              alt="Foto do desenvolvedor Victor Alves"
+              className="w-40 h-40 object-cover rounded-md"
+            />
+
+            <div className="p-5 flex flex-row justify-center gap-5">
+              <a href="https://www.linkedin.com/in/victor-alves-10587b2b6/" target="_blank">
+                <img src="/img/linkedin.png" alt="LinkedIn" className="w-10" />
+              </a>
+              <a href="https://github.com/victoralves10" target="_blank">
+                <img src="/img/github.png" alt="GitHub" className="w-10" />
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -5,12 +5,23 @@ export default function Principal() {
     <main className="flex flex-col items-center gap-8 p-6 bg-white flex-1">
       {/* Duas divs lado a lado em telas md+ e empilhadas no mobile */}
       <div className="flex flex-col md:flex-row gap-6 w-full">
-        <div className="flex-1 p-6 bg-gray-200 rounded-md shadow h-60"></div>
-        <div className="flex-1 p-6 bg-gray-200 rounded-md shadow h-60"></div>
+        <div className="flex-1 p-6 bg-gray-200 rounded-md shadow h-60">
+          <img
+            src="/img/sophi.png"        // coloque a imagem em public/img/
+            alt="Imagem do Chatbot Sophi"
+            className="w-32 h-32 mb-4"
+          />
+          <h2 className="text-2xl font-semibold mb-2">Chatbot Sophie</h2>
+        </div>
+
+        <div className="flex-1 p-6 bg-gray-200 rounded-md shadow h-60">
+
+        </div>
       </div>
 
       <button className="bg-red-700 text-white px-6 py-3 rounded-md shadow hover:border-black ">
         Confira os nossos produtos
       </button>
-    </main>)
-    }
+    </main>
+  )
+}
